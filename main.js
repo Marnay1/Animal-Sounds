d8bc6ad1547c1c6d2217a4a676b3257656bb510d
Marnay1 <87884789+Marnay1@users.noreply.github.com>
2022-03-03 23:06:19 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-https://teachablemachine.withgoogle.com/models/qIe7-tdaQ/
+https://teachablemachine.withgoogle.com/models/qIe7-tdaQ/model.json
+function start(){
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier= ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/qIe7-tdaQ/model.json', modelReady);
+}
+function modelReady(){
+    classifier.classify(gotResults);
+}
+function gotResults(error, results){
+    console.log("Got Result");
+}
